67674e9b38c3997fa02564d6ab1e73c52ba657da
William Rockwood <wrockwood@tadl.org>
2026-04-10 10:49:23 -0400

    Expand about page content

--- views/About/Index.php
+++ views/About/Index.php
@@ -8,16 +8,34 @@
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-sm-8">
+		<div class="col-sm-12">
 			<h2>About the Local History Collection</h2>
 			<h3>Archival Context Statement</h3>
 			<p>The Local History Collection at TADL is responsible for preserving and maintaining access to the permanent, historical records of Grand Traverse County.</p>
 			<p>This website and the archival records it describes may include content and language that is upsetting or offensive. Records reflect the language and attitudes of the historical period in which they were created.</p>
 			<p>This can include images or language that is racist, sexist, ableist, homophobic, and otherwise offensive or discriminatory. These materials are preserved for their historical significance and views that may be expressed within them are not endorsed by either the Traverse Area District Library (TADL) or the Local History Collection.</p>
-		</div>
-		<div class="col-sm-3 col-sm-offset-1">
-			<address>Archives<br>			100 Second Avenue, 2nd floor<br>			New York, NY 10010</address>
-		
-			<address>Jennifer Smith, Archivist<br>			<span class="info">Phone</span> — 212 222.2222<br>			<span class="info">Fax</span> — 212 222.2223<br>			<span class="info">Email</span> — <a href="#">email@archive.edu</a></address>
+
+			<h3>Research in Special Collections at the Traverse Area District Library</h3>
+			<p>Genealogy, local history, sheet music, microfilm and fiche, rare books... the list goes on! Not all of our materials are in our library catalog, so look to this page for a more complete picture of the wealth of resources available to you.</p>
+
+			<h3>Genealogy</h3>
+			<p>Visit the Genealogy collection on the second floor of the Main Library, Woodmere. Whether you are expert level or novice, this collection curated by the Grand Traverse Area Genealogical Society has something for you! In print, we have census indexes from all across the United States, Passenger and Ship Lists and Indexes, a monograph collection strong in the Midwest and Northeastern regions of the United States, and an extensive periodicals collection. For more information on our holdings in the Genealogy collection, visit the <a href="https://catalog.tadl.org">TADL Catalog</a>.</p>
+
+			<h3>Local History Vertical Files</h3>
+			<p>The library staff maintains a print vertical file collection of information about towns, cities, counties, events, people, landmarks, and other topics relating to Northwestern Michigan. The files contain pamphlets, newspaper clippings, correspondence with staff, articles, historical sketches, and other informational materials. For more information on our holdings in the collection, visit <a href="http://localhistory.tadl.org/vertfiles">Local History Vertical Files</a>.</p>
+
+			<h3>Microform</h3>
+			<p>On the second floor of the Woodmere Branch, popular periodicals, local history and genealogy resources and the local newspaper archives are available in microform. Readers are available for your convenience, with printing and scanning capabilities. Please visit <a href="http://localhistory.tadl.org/microform">Microform</a> for a list of holdings.</p>
+
+			<h3>Liz Bannister Sheet Music Collection</h3>
+			<p>This collection of over 2,000 pieces of full-sized sheet music was donated to the library by local music legend Elizabeth Bannister. Featuring popular music and showtunes from the turn of the 20th century to the 1960s, this collection is accessible on the second floor of the Woodmere Branch. As the collection is non-browsing and non-circulating, the librarians on staff will be happy to assist you with access. You can search for music in this collection in our catalog by title, or browse by searching for "Liz Bannister" as a keyword in the <a href="https://catalog.tadl.org">TADL Catalog</a>.</p>
+
+			<h3>How Can Materials Be Used from the Local History Collection?</h3>
+			<p>Most of the materials available in the Traverse Area District Library Local History Collection (<a href="http://localhistory.tadl.org/"><strong>localhistory.tadl.org</strong></a>) may be downloaded and reused without permission for purposes of research, private study, and education (non-commercial use). Any questions or concerns about the use of LHC materials should be directed to <a href="mailto:ask@tadl.org">ask@tadl.org</a>.</p>
+			<p>When using LHC materials, please credit "Traverse Area District Library Local History Collection" and include the catalog URL of the item to allow others to access the original material. Here is an example citation:</p>
+			<p><strong>Image courtesy of Traverse Area District Library Local History Collection, <a href="https://localhistory.tadl.org/items/show/14906">https://localhistory.tadl.org/items/show/14906</a>.</strong></p>
+
+			<h3>How Do I Use Local History Collection Materials in Published Works?</h3>
+			<p>In order to use materials from the Local History Collection for publication purposes, including signage, book publication, and documentaries, you will need to <a href="https://www.tadl.org/sites/default/files/2024-06/TADL_LHC_pubagmt.pdf"><strong>complete the publication agreement form</strong></a>. Email the completed form to <a href="mailto:localhistory@tadl.org">localhistory@tadl.org</a>.</p>
 		</div>
 	</div>
