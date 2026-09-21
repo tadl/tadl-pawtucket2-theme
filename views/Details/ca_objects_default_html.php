1bdd008b80c568584474d8f7072a315427f6610f
William Rockwood <wrockwood@tadl.org>
2026-09-21 11:16:04 -0400

    Use browser history for object Back link
    Avoid adding a duplicate collection results entry when returning from an object detail page, which otherwise traps collection and object Back links in a loop. Retain the prior results URL as the non-JavaScript fallback.

--- views/Details/ca_objects_default_html.php
+++ views/Details/ca_objects_default_html.php
@@ -35,14 +35,19 @@
 	$vn_share_enabled = 	$this->getVar("shareEnabled");
 	$vn_pdf_enabled = 		$this->getVar("pdfEnabled");
 	$vn_id =				$t_object->get('ca_objects.object_id');
+	$vs_results_url = 		$this->getVar('resultsURL');
+	if (!$vs_results_url) {
+		$vs_results_url = caNavUrl($this->request, '', 'Browse', 'objects');
+	}
+	$vs_history_back_link = '<a href="'.htmlspecialchars($vs_results_url, ENT_QUOTES, 'UTF-8').'" aria-label="'.htmlspecialchars(_t('Back'), ENT_QUOTES, 'UTF-8').'" onclick="if (window.history.length > 1) { window.history.go(-1); return false; }"><i class="fa fa-angle-double-left" aria-hidden="true"></i><div class="small">'.htmlspecialchars(_t('Back'), ENT_QUOTES, 'UTF-8').'</div></a>';
 ?>
 <div class="row">
 	<div class='col-xs-12 navTop'><!--- only shown at small screen size -->
-		{{{previousLink}}}{{{resultsLink}}}{{{nextLink}}}
+		{{{previousLink}}}<?php print $vs_history_back_link; ?>{{{nextLink}}}
 	</div><!-- end detailTop -->
 	<div class='navLeftRight col-xs-1 col-sm-1 col-md-1 col-lg-1'>
 		<div class="detailNavBgLeft">
-			{{{previousLink}}}{{{resultsLink}}}
+			{{{previousLink}}}<?php print $vs_history_back_link; ?>
 		</div><!-- end detailNavBgLeft -->
 	</div><!-- end col -->
 	<div class='col-xs-12 col-sm-10 col-md-10 col-lg-10'>
